09cea37b8a74cc390963a6a9fed93fdc7f909d29
hzhero1 <38153901+hzhero1@users.noreply.github.com>
2022-07-16 16:03:28 +0800

    first commit，项目搭建，前端整合测试

---
+++ src/main/java/ltd/starlight/mall/StarlightMallApplication.java
@@ -0,0 +1,13 @@
+package ltd.starlight.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarlightMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarlightMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ltd/starlight/mall/StarlightMallApplicationTests.java
@@ -0,0 +1,13 @@
+package ltd.starlight.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarlightMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
